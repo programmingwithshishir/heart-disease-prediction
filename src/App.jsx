c5e25b481b2c67dfc4d8b51d00cfa8d807ae9a7c
Shishir Shetty <programmingwithshishir@gmail.com>
2025-03-23 12:38:37 +0530

    Add a login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./components/Login";
+import HomePage from "./components/Home";
+
 function App() {
   return (
-    <h1 className="text-amber-500">Hello World</h1>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/" element={<HomePage/>} />
+      </Routes>
+    </Router>
   )
 }
 
